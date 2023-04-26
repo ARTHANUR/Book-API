2ab4db7dab49305f4dc760c16f55d86a3352f050
shashank <sarthanur@gmail.com>
2023-04-27 00:15:12 +0530

    API search added

--- src/main/Card.jsx
+++ src/main/Card.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import "./Card.css"
+import React from "react";
+import "./Card.css";
 
-const Card = () => {
-  return (
-    <>
-      <div className="card-section">
-        <img src="./image.png" alt="" />
-        <h1>Name</h1>
-        <h3>$69</h3>
-      </div>
-    </>
-  )
-}
+const Card = ({ book }) => {
+    console.log(book);
+    return (
+        <>
+            {book.map((item) => {
+              let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail
+                return (
+                    <>
+                        <div className="card-section">
+                            <img src={thumbnail} alt="" />
+                            <h1>Name</h1>
+                            <h3>$69</h3>
+                        </div>
+                    </>
+                );
+            })}
+        </>
+    );
+};
 
-export default Card
+export default Card;
--- src/main/Home.jsx
+++ src/main/Home.jsx
@@ -1,26 +1,32 @@
 import React , {useState} from "react";
 import Card from "./Card";
 import "./Home.css"
+import axios from "axios";
 
 const Home = () => {
 
 const [search , setSearch] = useState("")
+const [bookData , setData] = useState([])
 
 
-const searchBook = () => {
-    console.log("hello")
+const searchBook = (evt) => {
+    if (evt.key==="Enter")
+        {
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyAMVPrrtH0JZygn502ZaZbqD0rMmvr88P8'+'&maxResults=40')
+            .then(res => setData(res.data.items))
+            .catch(err => console.log(err))
+        }
+    
 }
 
     return (
         <>
             <div className="home">
                 <div className="input-section">
-                    <input type="text" value={search} onChange={ (e) => {setSearch(e.target.value)} }   />
-                    <button  onClick={searchBook} > + </button>
+                    <input type="text" value={search} onChange={ (e) => {setSearch(e.target.value)} } onKeyPress={searchBook}   />
                 </div>
                 <div className="result-section">
-                    <Card />
-                    
+                    {<Card book = {bookData} />}
                 </div>
             </div>
         </>
