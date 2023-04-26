742596d406dbcf93ad6aab3f16a4251b29b85991
shashank <sarthanur@gmail.com>
2023-04-27 02:13:17 +0530

    Cascading done

--- src/main/Card.jsx
+++ src/main/Card.jsx
@@ -6,22 +6,29 @@ const Card = ({ book }) => {
     return (
         <>
             {book.map((item) => {
-              let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail
-              let amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
-              let title = item.volumeInfo.title
+                let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
+                let amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
+                let title = item.volumeInfo.title;
 
-              if (thumbnail != undefined && amount != undefined)
-                {
-                  return (
-                      <>
-                          <div className="card-section">
-                              <img src={thumbnail} alt="" />
-                              <h1>{title}</h1>
-                              <h3>Rs. {amount}</h3>
-                          </div>
-                      </>
-                  );
-
+                if (thumbnail != undefined && amount != undefined) {
+                    return (
+                        <>
+                            <div className="card-section">
+                                <img src={thumbnail} alt="" />
+                                <h1>{title}</h1>
+                                <h3>- {item.volumeInfo.authors[0]}</h3>
+                                <div>
+                                    <h3>
+                                        {" "}
+                                        <em> &#8377;</em> {amount}
+                                    </h3>
+                                    <p>
+                                        {item.volumeInfo.pageCount} <em> pages </em>
+                                    </p>
+                                </div>
+                            </div>
+                        </>
+                    );
                 }
             })}
         </>
--- src/main/Home.jsx
+++ src/main/Home.jsx
@@ -1,33 +1,37 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import Card from "./Card";
-import "./Home.css"
 import axios from "axios";
+import "./Card.css";
 
 const Home = () => {
+    const [search, setSearch] = useState("");
+    const [bookData, setData] = useState([]);
 
-const [search , setSearch] = useState("")
-const [bookData , setData] = useState([])
-
-
-const searchBook = (evt) => {
-    if (evt.key==="Enter")
-        {
-            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyAMVPrrtH0JZygn502ZaZbqD0rMmvr88P8'+'&maxResults=40')
-            .then(res => setData(res.data.items))
-            .catch(err => console.log(err))
+    const searchBook = (evt) => {
+        if (evt.key === "Enter") {
+            axios
+                .get("https://www.googleapis.com/books/v1/volumes?q=" + search + "&key=AIzaSyAMVPrrtH0JZygn502ZaZbqD0rMmvr88P8" + "&maxResults=40")
+                .then((res) => setData(res.data.items))
+                .catch((err) => console.log(err));
         }
-    
-}
+    };
 
     return (
         <>
             <div className="home">
+                <h1>Book Search</h1>
                 <div className="input-section">
-                    <input type="text" value={search} onChange={ (e) => {setSearch(e.target.value)} } onKeyPress={searchBook}   />
+                    <input
+                        placeholder=" &#128270; Enter The Book Name"
+                        type="text"
+                        value={search}
+                        onChange={(e) => {
+                            setSearch(e.target.value);
+                        }}
+                        onKeyPress={searchBook}
+                    />
                 </div>
-                <div className="result-section">
-                    {<Card book = {bookData} />}
-                </div>
+                <div className="result-section">{<Card book={bookData} />}</div>
             </div>
         </>
     );
